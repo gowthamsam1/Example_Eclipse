e19f5ffa1316ae7b8a5de59e26d48fa06cb3790d
gowthamsam1 <155748124+gowthamsam1@users.noreply.github.com>
2024-01-08 21:52:37 +0530

    new file from my desktop

---
+++ Sampleone.java
@@ -0,0 +1,14 @@
+package selenium;
+
+public class Sampleone {
+
+	public static void main(String[] args) {
+		
+		String []call= {"mani","Gowtham","viki"};
+		
+		for(String t: call) {
+			System.out.println(t);
+		}
+	}
+
+}
